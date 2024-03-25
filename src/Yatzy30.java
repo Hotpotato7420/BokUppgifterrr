6e4c21e3f6ac359a00d80db39e3d44cbc286ad43
Hotpotato7420 <axejak056@gmail.com>
2024-03-25 13:09:11 +0100

    commit

--- src/Yatzy30.java
+++ src/Yatzy30.java
@@ -1,28 +1,82 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Yatzy30 {
+
+
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hur många spelare är det?");
+        int players = in.nextInt();
+
+        
+        for (int i = 1; i <= players; i++){
+            System.out.println("Vad heter spelare " + i + "?");
+
+        }
+        //String[] playerList
+
+        diceRoll();
+        System.out.println("You rolled a " + diceRoll());
+    }
+
+
+    public static int diceRoll(){
+
+    int dice;
+    int randomNum = (int)(Math.random() * 6);
+    if (randomNum <= 1){
+        dice = 1;
+    }
+    else if (randomNum == 2){
+        dice = 2;
+    }
+    else if (randomNum == 3){
+        dice = 3;
+    }
+    else if (randomNum == 4){
+        dice = 4;
+    }
+    else if (randomNum == 5){
+        dice = 5;
+    }
+    else {
+        dice = 6;
+    }
+
+
+        return dice;
+    }
+
+
+
+
+
+
         // Skapa en Scanner som kan ta in input
 
-        // FrÃ¥ga anvÃ¤ndaren hur mÃ¥nga spelaren och dess namn.
-        // Skapa en arrayList som sparar antal anvÃ¤ndare och dess namn.
-        // Skapa en variabel fÃ¶r spelarens poÃ¤ng och ge 30 poÃ¤ng till alla spelare
+        // Fråga användaren hur många spelaren och dess namn.
+        // Skapa en arrayList som sparar antal användare och dess namn.
+        // Skapa en variabel för spelarens poäng och ge 30 poäng till alla spelare
         // Ankalla metoden Spel
 
         // Metod spel
-        // Skapa variabel fÃ¶r talet som man slÃ¥r
-        // Skapa variabel fÃ¶r antalet tÃ¤rningar kvar.
-        // Skapa tÃ¤rningar som spelaren ska slÃ¥, typ anvÃ¤nda random funktionen och ta 1/6 = 1, 2/6 = 2, 3/6 = 3 osv.
-        // Spelaren slÃ¥r tÃ¤rningarna och vÃ¤ljer vilka den vill ha kvar, tvinga spelaren att vÃ¤lja en. Ta bort lika mÃ¥nga tÃ¤rningar som man vÃ¤ljer att ha kvar.
-        // Spelaren gÃ¶r om det tills att den har inga tÃ¤rningar kvar.
-        // Ifall spelaren fÃ¥r Ã¶ver 30, ankalla metoden battlePhase
-        // Annars gÃ¥ vidare till nÃ¤sta spelare och ta bort poÃ¤ng beroende pÃ¥ hur mycket ifrÃ¥n 30 dem var.
+        // Skapa variabel för talet som man slår
+        // Skapa variabel för antalet tärningar kvar.
+        // Skapa tärningar som spelaren ska slå, typ använda random funktionen och ta 1/6 = 1, 2/6 = 2, 3/6 = 3 osv.
+        // Spelaren slår tärningarna och väljer vilka den vill ha kvar, tvinga spelaren att välja en. Ta bort lika många tärningar som man väljer att ha kvar.
+        // Spelaren gör om det tills att den har inga tärningar kvar.
+        // Ifall spelaren får över 30, ankalla metoden battlePhase
+        // Annars gå vidare till nästa spelare och ta bort poäng beroende på hur mycket ifrån 30 dem var.
 
         // Metod battlePhase
-        // FrÃ¥ga anvÃ¤ndaren vilken spelare den vill attackera.
+        // Fråga användaren vilken spelare den vill attackera.
         // Skapa variabel battleTal (talet - 30)
-        // SlÃ¥ tÃ¤rningarna fÃ¶r talet du behÃ¶ver (1 fÃ¶r 31, 2 fÃ¶r 32 etc.)
-        // Ifall spelaren fÃ¥r det talet som den behÃ¶ver sÃ¥ spara den och slÃ¥ om resten av tÃ¤rningarna tills spelaren inte slÃ¥r sitt tal.
-        // Ifall du slÃ¥r ett battleTal sÃ¥ ta bort sÃ¥ mÃ¥nga poÃ¤ng som antalet tÃ¤rningar * battleTal frÃ¥n spelaren som du vill attackera. Annars gÃ¥ till nÃ¤sta steg.
-        // GÃ¥ tillbaka till spel metoden och gÃ¥ till nÃ¤sta spelare.
+        // Slå tärningarna för talet du behöver (1 för 31, 2 för 32 etc.)
+        // Ifall spelaren får det talet som den behöver så spara den och slå om resten av tärningarna tills spelaren inte slår sitt tal.
+        // Ifall du slår ett battleTal så ta bort så många poäng som antalet tärningar * battleTal från spelaren som du vill attackera. Annars gå till nästa steg.
+        // Gå tillbaka till spel metoden och gå till nästa spelare.
 
-    }
 }
